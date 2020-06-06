9e8ab4c16dfb70f5854d322f82ea64ddff95f016
sergioleonurrutia <3250762511401@ingenieria.usac.edu.gt>
2020-06-05 20:25:28 -0600

    Hice una funcion para el numero perfecto

--- Tarea1/Tarea1.py
+++ Tarea1/Tarea1.py
@@ -1,21 +1,30 @@
+#****************** FUNCION *************************
+def numero_perfecto(N):
+    suma=0
+    for i in range(1,N+1):
+        cont=0
+        for j in range(1,i):
+            modulo=i%j
+            if modulo==0:
+                cont=cont+j
+            suma=cont
+        if suma==i:
+            print("El numero "+str(i)+ " es perfecto")
+#****************************************************
+
 print('''
+---------------------------------------------------------------------
 ***Bienvenidos al programa que determina si un numero es perfecto***
+Programador: Sergio Augusto León Urrutia
+Carnet: 201700722
+---------------------------------------------------------------------
 ''')
-print("\n\n\n")
-N=int(input("Ingrese un numero entero: "))
-lista=list(range(0,N))
-print("\n\n\n")
-print("EL numero ingresado es: "+ str(N))
-print("\n\n\n")
-suma=0
-for i in range(1,N+1):
-    cont=0
-    for j in range(1,i):
-        modulo=i%j
-        if modulo==0:
-            cont=cont+j
-            #print("posicion" + str(i) + " J: "+str(j))
-            #print("Suma: "+ str(cont))
-        suma=cont
-    if suma==i:
-        print("El numero "+str(i)+ " es perfecto")
+
+print("\n")
+numero=int(input("Ingrese un numero entero: "))
+lista=list(range(0,numero))
+print("\n\n")
+print("EL numero ingresado es: "+ str(numero))
+print("\n\n")
+
+numero_perfecto(numero)
