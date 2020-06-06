8bb3783c23e3481e1045bec9367432e499e77113
sergioleonurrutia <3250762511401@ingenieria.usac.edu.gt>
2020-06-05 20:06:19 -0600

    Mejore el programa de los numeros perfectos

--- Tarea1/Tarea1.py
+++ Tarea1/Tarea1.py
@@ -1,18 +1,21 @@
+print('''
+***Bienvenidos al programa que determina si un numero es perfecto***
+''')
+print("\n\n\n")
 N=int(input("Ingrese un numero entero: "))
-lista=list(range(1,N))
-suma=0
-cont=0
+lista=list(range(0,N))
+print("\n\n\n")
 print("EL numero ingresado es: "+ str(N))
-print("La lista de numeros desde 0 hasta N es: " + str(lista))
-
-for i in range(0,len(lista)):
+print("\n\n\n")
+suma=0
+for i in range(1,N+1):
     cont=0
     for j in range(1,i):
         modulo=i%j
         if modulo==0:
             cont=cont+j
-            print("posicion" + str(i) + " J: "+str(j))
-            #suma=cont+suma
-            print("Suma: "+ str(cont))
-        if suma==i:
-            print("El numero "+str(i)+ " es perfecto")
+            #print("posicion" + str(i) + " J: "+str(j))
+            #print("Suma: "+ str(cont))
+        suma=cont
+    if suma==i:
+        print("El numero "+str(i)+ " es perfecto")
