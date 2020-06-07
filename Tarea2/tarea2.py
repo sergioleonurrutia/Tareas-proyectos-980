b63daa788f6940f316248023526eef0e535ccb2c
sergioleonurrutia <3250762511401@ingenieria.usac.edu.gt>
2020-06-06 19:24:47 -0600

    Termine la tarea2, solo optimizarla

--- Tarea2/tarea2.py
+++ Tarea2/tarea2.py
@@ -10,14 +10,26 @@ def divisores(N):
 
 cont=2
 bandera=0
+temp1=0
+temp2=0
 numero= int(input("Ingrese un numero entero: "))
 
-while bandera<=numero:
+while bandera<numero:
     amigo1=divisores(cont)
-    print("Respuesta del primer numero: " +str(amigo1))
+    #print("Respuesta del primer numero: " +str(amigo1))
     amigo2=divisores(amigo1)
-    print("Respuesta del segundo numero: " + str(amigo2))
-    cont=cont+1
-
+    #print("Respuesta del segundo numero: " + str(amigo2))
+    #print("Contador: "+str(cont))
+    
     if amigo1==amigo2:
+        cont=cont+1
+
+    if (temp2==amigo1 and temp1==amigo2):
+        cont=cont+1
+
+    if cont==amigo2:
         bandera=bandera+1
+        print("Los numeros "+str(amigo1)+" y "+str(amigo2)+" son los amigos No. "+str(bandera))
+        temp1=amigo1
+        temp2=amigo2  
+    cont=cont+1
